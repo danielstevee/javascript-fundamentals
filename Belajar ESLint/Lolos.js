a6a2e1876f0032ad193496282b8a6e4562ce2293
Daniel Steven Setyawan <nifangzuo@gmail.com>
2026-09-23 23:11:24 +0700

    Belajar Form dan Event

--- Belajar ESLint/Lolos.js
+++ Belajar ESLint/Lolos.js
@@ -7,6 +7,12 @@
  * 3. Konfigurasi rules nya di eslint.config.js
  * 4. Cek konsistensi aturan code npx eslint fileName.js
  * 5. Jika Lolos tidak muncul output, jika tidak akan muncul code error problem tidak sesuai aturan estlint
+
+ * PERHATIAN = node_modules/ ❌ JANGAN push. file terlalu besar 
+ * Anggota lain hanya lakukan: 
+ *  git clone <repository>
+    cd project
+    npm install 
  */
 
 const nama = "Daniel";
